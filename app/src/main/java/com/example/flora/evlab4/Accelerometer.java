d096b0544076c862e6726027bf343b178f69d98e
flora-wl <weil.flora@gmail.com>
2016-08-12 20:05:10 +0800

    Update Accelerometer.java

--- app/src/main/java/com/example/flora/evlab4/Accelerometer.java
+++ app/src/main/java/com/example/flora/evlab4/Accelerometer.java
@@ -1,127 +1,150 @@
 package com.example.flora.evlab4;
 
+import android.app.Activity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Vibrator;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-        import android.app.Activity;
-        import android.content.Context;
-        import android.hardware.Sensor;
-        import android.hardware.SensorEvent;
-        import android.hardware.SensorEventListener;
-        import android.hardware.SensorManager;
-        import android.os.Bundle;
-        import android.widget.ImageView;
-        import android.widget.TextView;
+import org.w3c.dom.Text;
+
+import static java.lang.Math.acos;
+import static java.lang.Math.asin;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Vibrator;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+import static java.lang.Math.acos;
+import static java.lang.Math.asin;
+
+import android.app.Activity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 
-        import android.bluetooth.BluetoothSocket;
-        import android.content.Intent;
-        import android.view.View;
-        import android.widget.Button;
-        import android.widget.Toast;
-        import android.app.ProgressDialog;
-        import android.bluetooth.BluetoothAdapter;
-        import android.bluetooth.BluetoothDevice;
-        import android.os.AsyncTask;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import android.app.ProgressDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.os.AsyncTask;
 
-        import java.io.IOException;
-        import java.util.UUID;
+import java.io.IOException;
+import java.util.UUID;
 
 public class Accelerometer extends Activity implements SensorEventListener {
 
-    //Initialize Disconnect button
-    Button btnDis;
-
-    //Data sent via Bluetooth
-    String address = null;
-    String value1 = String.valueOf(0);
-    String value2 = String.valueOf(0);
-    String value3 = String.valueOf(0);
-    String values = String.valueOf(0);
-
     //Bluetooth settings
+    String address = null;
     private ProgressDialog progress;
     BluetoothAdapter myBluetooth = null;
     BluetoothSocket btSocket = null;
     private boolean isBtConnected = false;
 
+    //Send data via Bluetooth
+    String steeringAc = String.valueOf(0);
+    String throttleAc = String.valueOf(0);
+    String brakeAc = String.valueOf(0);
+    String send = String.valueOf(0);
+    String checksum = String.valueOf(0);
+    int checksum_int = 0;
+
     //SPP UUID
     static final UUID myUUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
 
-    //Accelerometer setup
     private SensorManager sensorManager;
     private Sensor accelerometer;
 
     private float x = 0;
     private float y = 0;
-    private float z = 0;
 
-    private float steering = 0;
-    private float throttle = 0;
-    private float brake = 0;
+    private int steering = 0;
+    private int throttle = 0;
+    private int brake = 0;
 
-    public boolean isHorizontal = true;
 
-    final static int middlePoint = 128;
+    double convert = 180 / Math.PI;
 
-    private TextView currentX, currentY, currentZ, steeringA, throttleA, brakeA;
+    public boolean forward = true;
+
+    public float angleZ_X, angleZ_Y, angleZCalc;
+
+    final static int middlePoint = 127;
+    final static float rangeN = 127;
+    final static float rangeP = 128;
+    final static float rangeSteering = 90;
+
+    private TextView currentY, currentZ,angleAY;
 
     ImageView logo;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
-
-        //Get address from main activity
-        Intent newint = getIntent();
-        address = newint.getStringExtra(MainActivity.EXTRA_ADDRESSA);
-
-        //Layout
-        setContentView(R.layout.activity_accelerometer);
-
-        //Initialize disconnect button
-        btnDis = (Button)findViewById(R.id.buttondis);
-
+        setContentView(R.layout.activity_main);
         initializeViews();
 
-        //Connect Bluetooth
-        new ConnectBT().execute();
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
 
-        //Implement disconnect button
-        btnDis.setOnClickListener(new View.OnClickListener() {
-            @Override
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+
+        final Button btn_changeDirection = (Button) findViewById(R.id.btn_changeDirection);
+        btn_changeDirection.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Disconnect();
+                changeDirection();
             }
         });
-
-        //Get Sensors
-        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        if (sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null) {
-            // success! we have an accelerometer
-
-            accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-            sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
-        } else {
-            // fai! we dont have an accelerometer!
-        }
-
     }
 
-    //initializeViews() display sensor values on app
     public void initializeViews() {
-        currentX = (TextView) findViewById(R.id.currentX);
         currentY = (TextView) findViewById(R.id.currentY);
         currentZ = (TextView) findViewById(R.id.currentZ);
 
-        steeringA = (TextView) findViewById(R.id.steeringA);
-        throttleA = (TextView) findViewById(R.id.throttleA);
-        brakeA = (TextView) findViewById(R.id.brakeA);
+        angleAY = (TextView) findViewById(R.id.angleAY);
+
+        logo = (ImageView) findViewById(R.id.makerbay_logo);
     }
 
     //onResume() register the accelerometer for listening the events
     protected void onResume() {
         super.onResume();
         sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        displayAngles();
+        displayCurrentValues();
     }
 
     //onPause() unregister the accelerometer for stop listening the events
@@ -130,107 +153,89 @@ public class Accelerometer extends Activity implements SensorEventListener {
         sensorManager.unregisterListener(this);
     }
 
+    protected void onDestroy() {
+        super.onDestroy();
+        sensorManager.unregisterListener(this);
+    }
+
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
     }
 
     @Override
     public void onSensorChanged(SensorEvent event) {
+        Sensor sensor = event.sensor;
+        x = event.values[0];//always configure it according to device, the following code is tailored to a landscape tablet
+        y = event.values[1];//x-axis along long side (positive direction is left <-- right), y-axis along short side (90° CCW to x)
+        //z-axis through display plane (positive direction is out of display)
 
-        // clean current values
-        displayCleanValues();
+        ///call methods only when certain threshold in change is reached to save computing power?
 
-        x = event.values[0];
-        y = event.values[1];
-        z = event.values[2];
+        calcAngleZ_XY();
+        calcSteering();
 
-        double noise = 1.5;
-        float noiseG = 10;
-        /*
-            initial values for horizontal:
-                x~=0.15-0.25
-                y~=0.0 - (-0.05)
-                z~=10
-                --------
-            initial values for vertical:
-                x~=0.3-0.4
-                y~=9.6-9.7
-                z~=0.2-0.3
-         */
-        if (x < noise && x > 0 || x > -noise && x < 0) {
-            x = 0;
-            //deltaX can be used for steering with tablet being held vertical or horizontal
-            //90° til to left -> deltaX ~= -10.2 || 90° tilt to the right -> deltaX ~= 9.3
-        }
-
-        if (y > noiseG - noise && y < noiseG + noise){
-            y = noiseG;
-        }
-        else if (y < noise && y > 0 || y > -noise && y < 0) {
-            y = 0;
-            //deltaY can only be used for accel/braking with tablet being horizontal
-            //90° tilt towards user -> deltaY ~= -9.8 || 90° tilt away from user -> deltaY ~= 9.8
-        }
-
-        if (z > noiseG - noise && z < noiseG + noise) {
-            z = noiseG;
-        }
-        else if(z < noise && z > 0 || z > -noise && z < 0){
-            z = 0;
-        }
-        // display the current x,y,z accelerometer values
         displayCurrentValues();
-
-        calcMotor();
-        displayMotor();
-        addValues();
-
-
+        displayAngles();
     }
 
-    public void displayCleanValues() {
-        currentX.setText("0.0");
-        currentY.setText("0.0");
-        currentZ.setText("0.0");
 
-        steeringA.setText("0.0");
-        throttleA.setText("0.0");
-        brakeA.setText("0.0");
-    }
+    void calcAngleZ_XY() { //calculate rotation around z-axis, based on gravitation along x and y
+        double xBaseP = 10.25;
+        double xBaseN = 9.35;
+        double yBaseP = 9.65;
 
-    // display the current x,y,z accelerometer values
-    public void displayCurrentValues() {
-        currentX.setText(Float.toString(x));
-        currentY.setText(Float.toString(y));
-        currentZ.setText(Float.toString(z));
+        double xPrec1 = 3.5;
+        double xPrec2 = -3.8;
+        double yPrec1 = 3.2;
+        double yPrec2 = 3.5;
 
-
-    }
-
-    public void calcMotor() {
         if (x > 0) {
-            steering = middlePoint - (x * 11);
+            if (x < xPrec1) {
+                angleZCalc = (float) (asin(x / xBaseP) * convert);
+            } else if (y < yPrec1) {
+                angleZCalc = (float) (acos(y / yBaseP) * convert);
+            } else
+                angleZCalc = (float) convert * (float) ((asin(x / xBaseP) + (acos(y / yBaseP))) / 2);
         } else if (x < 0) {
-            steering = middlePoint - (x * 10);
-        } else steering = 0;
-
-        if (z < 0) { //tilting towards user
-            throttle = 0;
-            brake = -(z * 20);
-        } else if (z > 0) {
-            throttle = z * 20;
-            brake = 0;
-        } else {
-            throttle = 0;
-            brake = 0;
-        }
+            if (x > xPrec2) {
+                angleZCalc = (float) (asin(x / xBaseN) * convert);
+            } else if (y < yPrec2) {
+                angleZCalc = - (float) (acos(y / yBaseP) * convert);
+            } else
+                angleZCalc = (float) convert * (float) ((asin(x / xBaseP) - (acos(y / yBaseP))) / 2);
+        } else
+            angleZCalc = 0;
     }
 
-    public void displayMotor() {
-        steeringA.setText(Float.toString(steering));
-        throttleA.setText(Float.toString(throttle));
-        brakeA.setText(Float.toString(brake));
+    void calcSteering() {
+        if (angleZCalc > 0) {
+            int steeringLeft = Math.round(angleZCalc * rangeP / rangeSteering);
+            if (steeringLeft > rangeN){
+                steering = 0;
+            } else steering = middlePoint - steeringLeft;
+        }
+        else if (angleZCalc < 0){
+            int steeringRight = - Math.round(angleZCalc * rangeN / rangeSteering);
+            if (steeringRight > rangeP){
+                steering = 255;
+            } else steering = middlePoint + steeringRight;
+        } else steering = middlePoint;
+    }
+
+    void changeDirection() {
+        forward = !forward;
+        if (forward) {
+            ((Button) findViewById(R.id.btn_changeDirection)).setText("Direction: forward");
+        } else ((Button) findViewById(R.id.btn_changeDirection)).setText("Direction: backward");
+    }
+
+    void displayAngles() {
+        angleAY.setText(String.valueOf(angleZCalc));
+    }
+
+    void displayCurrentValues() {
+        currentY.setText("steering  --  " + String.valueOf(steering));
+        currentZ.setText(String.valueOf(x) + "x---------y" + String.valueOf(y));
     }
 
     private void msg(String s)
@@ -243,14 +248,18 @@ public class Accelerometer extends Activity implements SensorEventListener {
     private void addValues() {
         if (btSocket!=null) {
 
-            value1 = Float.toString(steering);
-            value2 = Float.toString(throttle);
-            value3 = Float.toString(brake);
+            steeringAc = Float.toString(steering);
+            throttleAc = Float.toString(throttle);
+            brakeAc = Float.toString(brake);
 
-            values = value1+","+value2+","+value3+"#";
+            checksum_int = Integer.parseInt(steeringAc)+Integer.parseInt(throttleAc)+Integer.parseInt(brakeAc);
+            checksum = String.valueOf(checksum);
+
+            send = steeringAc+","+throttleAc+","+brakeAc+","+checksum+"#";
+
 
             try {
-                btSocket.getOutputStream().write(values.toString().getBytes());
+                btSocket.getOutputStream().write(send.toString().getBytes());
                 btSocket.getOutputStream().flush();
             } catch (IOException e) {
                 msg("Error");
